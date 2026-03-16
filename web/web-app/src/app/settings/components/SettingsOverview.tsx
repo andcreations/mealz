6daf2d1b304d2c21fcd555ac4547515186f34c19
Mikolaj Gucki <24221859+mikolajgucki@users.noreply.github.com>
2026-03-16 06:56:35 +0100

    feat(): add user info to the settings overview

--- web/web-app/src/app/settings/components/SettingsOverview.tsx
+++ web/web-app/src/app/settings/components/SettingsOverview.tsx
@@ -4,17 +4,25 @@ import { useNavigate } from 'react-router-dom';
 import { GWUserInfo } from '@mealz/backend-users-crud-gateway-api';
 
 import { Log } from '../../log';
+import { LoadStatus } from '../../common';
 import { PathTo } from '../../routing';
 import { usePatchState, useService } from '../../hooks';
 import { AuthService } from '../../auth';
 import { UserService } from '../../user';
 import { useTranslations } from '../../i18n';
-import { MaterialIcon } from '../../components';
+import { 
+  htmlToReact,
+  LoaderByStatus, 
+  LoaderSize, 
+  LoaderType, 
+  MaterialIcon,
+} from '../../components';
 import { SettingsOverviewTranslations } from './SettingsOverview.translations';
 import { SettingsMenu } from './SettingsMenu';
 import { GoToSettingsMenuItem } from './GoToSettingsMenuItem';
 
 interface SettingsOverviewState {
+  loadStatus: LoadStatus;
   userInfo?: GWUserInfo;
 }
 
@@ -24,7 +32,9 @@ export function SettingsOverview() {
   const userService = useService(UserService);
   const navigate = useNavigate();
 
-  const [state, setState] = React.useState<SettingsOverviewState>({});
+  const [state, setState] = React.useState<SettingsOverviewState>({
+    loadStatus: LoadStatus.Loading,
+  });
   const patchState = usePatchState(setState);
 
   // initial read
@@ -33,9 +43,10 @@ export function SettingsOverview() {
       () => userService.readCurrentUserV1(),
       'Failed to check if signed in',
     ).then(({ userInfo }) => {
-      patchState({ userInfo });
+      patchState({ loadStatus: LoadStatus.Loaded, userInfo });
     }).catch((error) => {
       Log.error('Failed to read current user', error);
+      patchState({ loadStatus: LoadStatus.FailedToLoad });
     });
   }, []);
 
@@ -55,52 +66,86 @@ export function SettingsOverview() {
     navigate(PathTo.calculatorSettings());
   };
 
+  const loader = {
+    type: () => {
+      return state.loadStatus === LoadStatus.FailedToLoad
+        ? LoaderType.Error
+        : LoaderType.Info;
+    },
+    subTitle: () => {
+      return state.loadStatus === LoadStatus.FailedToLoad
+        ? translate('failed-to-load')
+        : undefined;
+    },
+  };
+
   const onSignOut = () => {
     authService.signOutOrLogError();
     navigate(PathTo.signIn());
   };
 
   return (
-    <div className='mealz-settings-overview'>
-      <div className='mealz-settings-overview-user-info'>
+    <>
+      <LoaderByStatus
+        loadStatus={state.loadStatus}
+        size={LoaderSize.Small}
+        type={loader.type()}
+        subTitle={loader.subTitle()}
+      />
+      { state.loadStatus === LoadStatus.Loaded &&
+        <div className='mealz-settings-overview'>
+          <div className='mealz-settings-overview-user-info'>
+            <MaterialIcon
+              className='mealz-settings-overview-user-info-icon'
+              icon='person'
+            />
+            <div className='mealz-settings-overview-user-info-name'>
+              { state.userInfo?.firstName }
+            </div>
+            <div className='mealz-settings-overview-user-info-email'>
+              { state.userInfo?.email }
+            </div>
+          </div>
 
-      </div>
-      <SettingsMenu>
-        <GoToSettingsMenuItem
-          icon='water_drop'
-          label={translate('hydration-daily-plan')}
-          onClick={onHydrationDailyMealClick}
-        />
-        <GoToSettingsMenuItem
-          icon='view_agenda'
-          label={translate('daily-meal-plan')}
-          onClick={onDailyMealPlanClick}
-        />
-        <GoToSettingsMenuItem
-          icon='send'
-          label={translate('telegram')}
-          onClick={onTelegramSettingsClick}
-        />
-        <GoToSettingsMenuItem
-          icon='calculate'
-          label={translate('calculator')}
-          onClick={onCalculatorSettingsClick}
-        />
-      </SettingsMenu>
+          <div className='mealz-settings-overview-separator'></div>
+          <SettingsMenu>
+            <GoToSettingsMenuItem
+              icon='water_drop'
+              label={translate('hydration-daily-plan')}
+              onClick={onHydrationDailyMealClick}
+            />
+            <GoToSettingsMenuItem
+              icon='view_agenda'
+              label={translate('daily-meal-plan')}
+              onClick={onDailyMealPlanClick}
+            />
+            <GoToSettingsMenuItem
+              icon='send'
+              label={translate('telegram')}
+              onClick={onTelegramSettingsClick}
+            />
+            <GoToSettingsMenuItem
+              icon='calculate'
+              label={translate('calculator')}
+              onClick={onCalculatorSettingsClick}
+            />
+          </SettingsMenu>
 
-      <div className='mealz-settings-overview-sign-out-separator'></div>
-      <div
-        className='mealz-settings-overview-sign-out'
-        onClick={onSignOut}
-      >
-        <MaterialIcon
-          className='mealz-settings-overview-sign-out-icon'
-          icon='power_settings_new'
-        />
-        <div className='mealz-settings-overview-sign-out-label'>
-          { translate('sign-out') }
+          <div className='mealz-settings-overview-separator'></div>
+          <div
+            className='mealz-settings-overview-sign-out'
+            onClick={onSignOut}
+          >
+            <MaterialIcon
+              className='mealz-settings-overview-sign-out-icon'
+              icon='power_settings_new'
+            />
+            <div className='mealz-settings-overview-sign-out-label'>
+              { translate('sign-out') }
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      }
+    </>
   );
 }
