294b62e2d2e44cbb5606e7b227717602f88b79ed
Mikolaj Gucki <24221859+mikolajgucki@users.noreply.github.com>
2026-04-10 10:43:00 +0200

    fix(): fix translation

--- backend/backend-server/src/domains/meals/services/named/service/services/MealsNamedShareService.translations.ts
+++ backend/backend-server/src/domains/meals/services/named/service/services/MealsNamedShareService.translations.ts
@@ -3,7 +3,7 @@ import { Translations } from '@mealz/backend-common';
 export const MealsNamedShareServiceTranslations: Translations = {
   'user-shared-named-meal': 'User {0} shared named meal "{1}" with you',
   'accept': 'Add to my named meals',
-  'user-created-named-meal': 'Add meal "{0}" to your named meals',
+  'user-created-named-meal': 'Added meal "{0}" to your named meals',
   'named-meal-not-found': 'Something went wrong. We couldn\'t find the meal.',
   'meal-not-found': 'Something went wrong. We couldn\'t find the meal.',
 };
